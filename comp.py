589babdbded52e552c620847fbd0e6593084ab8b
Mahir Barot <46795655+mahirbarot@users.noreply.github.com>
2023-05-23 02:17:02 +0530

    added

---
+++ comp.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+# Fetch the service account key JSON file from Firebase Console
+cred = credentials.Certificate(r"serviceAccount.json")
+
+# # Initialize the Firebase app
+# firebase_admin.initialize_app(cred, {
+#     'databaseURL': 'https://smartdustbin-nuv-default-rtdb.firebaseio.com/'
+# })
+
+# Reference to the root of the database
+ref = db.reference('/complaints')
+
+st.set_page_config(
+        page_title="mini project",
+        page_icon="house_buildings",
+        layout='centered',
+    )
+
+
+
+def main():
+    st.title('Firebase Submission')
+
+    # Input fields
+    dustbin_id = st.text_input('Dustbin ID')
+    pincode = st.text_input('Pincode')
+    subject = st.text_input('Subject')
+
+    # Submit button
+    if st.button('Submit'):
+        # Push the data to Firebase
+        submission = {
+            'dustbin_id': dustbin_id,
+            'pincode': pincode,
+            'subject': subject
+        }
+        ref.child('complaints').child(dustbin_id).push(submission)
+        st.success('Data submitted successfully!')
+
+if __name__ == '__main__':
+    main()
+
+
+
